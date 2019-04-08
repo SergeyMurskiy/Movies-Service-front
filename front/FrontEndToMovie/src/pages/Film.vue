284c9254da3ff4c4b4ed2e1c608edbf0fc600922
sergejmurskij <sm2198sm@gmail.com>
2019-04-08 18:39:39 +0300

    Initial commit

---
+++ front/FrontEndToMovie/src/pages/Film.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <Header></Header>
+        <div class="content">
+            <h3>{{film.title}}</h3>
+            <img class="poster-lg" src="../../public/img/posters/Марсианин.jpg">
+            <div class="description">
+                <div class="description-item"><b>Год: </b> {{film.year}}</div><hr>
+                <div class="description-item"><b>Страна: </b> {{film.country}}</div><hr>
+                <div class="description-item"><b>Жанр: </b> {{film.genre}}</div><hr>
+                <div class="description-item"><b>Режиссер: </b> </div><hr>
+                <div class="description-item"><b>Дата выхода: </b> {{film.release}}</div><hr>
+                <div class="description-item"><b>Бюджет: </b> {{film.budget}}</div><hr>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios'
+    export default {
+        name: "Film",
+        data() {
+            return {
+                film: '',
+                authorization: localStorage.getItem("Authorization")
+            }
+        },
+        mounted() {
+            axios.defaults.headers = {
+                'Content-Type': 'application/json',
+                Authorization: this.authorization
+            };
+            axios.get("http://localhost:8080/api/films/film", {
+                params:{
+                    id: this.$route.params.id
+                }
+            }).then((response) => {
+                this.film = response.data;
+                this.film.year = this.film.year.substring(0, 4);
+                this.film.release = this.film.release.substring(0, 10);
+            })
+        }
+    }
+</script>
+
+<style scoped>
+    .description {
+        margin-top: 25px;
+        font-size: 15px;
+    }
+</style>
---
+++ front/FrontEndToMovie/src/pages/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <Header/>
+        <div class="content">
+            <h3>Новинки:</h3>
+            <Swiper class="swiper"></Swiper>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Swiper from '../components/Swiper.vue'
+
+    export default {
+        components: {
+            Swiper
+        }
+    }
+</script>
+
+<style>
+    .swiper {
+    }
+</style>
